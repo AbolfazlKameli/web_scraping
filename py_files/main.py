ce6f0817f8968139caef6a13fcf3686dd63e5f20
abolfazl <abolfazlkameli0@gmail.com>
2024-09-08 12:24:19 +0330

    feature/learn: learn how to crawl tables.

--- py_files/main.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://en.wikipedia.org/wiki/Mao_Zedong'
-
-response = requests.get(url)
-content = BeautifulSoup(response.text, features='html.parser')
-print(content.select('li > a[title="Benjamin Tucker"]'))
